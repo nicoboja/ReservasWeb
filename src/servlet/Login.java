997b6763817b4d68dbc23b9b7e18a42efe7ae436
nicoboja <nico.boja@gmail.com>
2017-11-21 18:50:10 -0300

    .

---
+++ src/servlet/Login.java
@@ -0,0 +1,67 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controlers.CtrlABMPersona;
+import entity.Persona;
+
+/**
+ * Servlet implementation class start
+ */
+@WebServlet({"/login, /Login"})
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			String user=request.getParameter("user");
+			String pass=request.getParameter("pass");
+			
+			Persona per=new Persona();
+			per.setUss(user);
+			per.setPass(pass);
+			
+			CtrlABMPersona ctrl= new CtrlABMPersona();
+			
+			int pers=ctrl.logueo(per);
+			
+			request.setAttribute("listaPersonas", ctrl.getAll());
+			
+			request.getSession().setAttribute("user", pers);
+			
+			request.getRequestDispatcher("WEB-INF/welcome.jsp").forward(request, response);
+			//response.getWriter().append(user).append(" ").append(pass);
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		//doGet(request, response);
+	}
+	
+
+}
